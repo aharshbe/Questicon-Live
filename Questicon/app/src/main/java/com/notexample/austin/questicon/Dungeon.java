b6ee3e0d2379f7d48b9514fdffa0aa6a0f6d6be3
aharshbe <a.catawampus@gmail.com>
2016-06-14 14:28:27 -0700

    Adding info for the stuff before having to do a less

--- Questicon/app/src/main/java/com/notexample/austin/questicon/Dungeon.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/Dungeon.java
@@ -1,8 +1,11 @@
 package com.notexample.austin.questicon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -14,33 +17,92 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 import cz.msebera.android.httpclient.Header;
 
 public class Dungeon extends AppCompatActivity {
-    LinkedList<String> items;
-    ArrayAdapter<String> mAdapter;
-    ListView listView;
+    ArrayList<DungeonModel> dungeonModels;
+    CustomAdapterDungeon adapterDungeon;
+    JsonHttpResponseHandler jsonHttpResponseHandler = new JsonHttpResponseHandler() {
+
+
+        @Override
+        public void onSuccess(int statusCode, Header[] headers, JSONObject responseBody) {
+
+
+            try {
+                JSONArray jsonArray = responseBody.getJSONArray("zones");
+
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    final JSONObject dungeon = jsonArray.getJSONObject(i);
+
+                    if (!dungeon.has("name") || !dungeon.has(("description")) || !dungeon.has(("numPlayers")) ||
+                            !dungeon.has(("advisedMinLevel")) || !dungeon.has(("advisedMaxLevel")) ||
+                            !dungeon.has(("floors")) || !dungeon.has(("bosses")) || !dungeon.has("location"))
+                        continue;
+                    final DungeonModel dungeonModel = new DungeonModel(dungeon.get("name").toString(), dungeon.get("description").toString(),
+                            dungeon.get("numPlayers").toString(),
+
+                            dungeon.get("bosses").toString(), dungeon.get("location"), dungeon.getInt("advisedMaxLevel"),
+                            dungeon.getInt("advisedMinLevel"),
+                            dungeon.get("floors").toString());
+                    dungeonModels.add(dungeonModel);
+                    adapterDungeon.notifyDataSetChanged();
+//                    bossesDesciption = bosses.get("description").toString();
+
+                    ListView listViewDungeon = (ListView) findViewById(R.id.listViewDungeon);
+
+
+                    listViewDungeon.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            DungeonModel dungeon = dungeonModels.get(position);
+
+                            Intent myIntent = new Intent(Dungeon.this, BossesDetailView.class);
+                            myIntent.putExtra("position", position);
+                            myIntent.putExtra("des", dungeon.getDescriptionD());
+                            myIntent.putExtra("name", dungeon.getNameD());
+                            startActivity(myIntent);
+                        }
+                    });
+                }
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+        @Override
+        public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+            super.onFailure(statusCode, headers, throwable, errorResponse);
+            Toast.makeText(getApplicationContext(), "Process Not Successful",
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dungeon);
 
-        Log.d("<OnCreate>", "Working" );
-
-        dungeons();
+        dungeon();
     }
 
-    public void dungeons() {
+    public void dungeon() {
 
-
-        items = new LinkedList<>();
-        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
-        listView = (ListView) findViewById(R.id.listViewDungeon);
-        listView.setAdapter(mAdapter);
-//        editText = (EditText) findViewById(R.id.editText);
+        ListView listViewDungeon = (ListView) findViewById(R.id.listViewDungeon);
+        dungeonModels = new ArrayList<>();
+        adapterDungeon = new CustomAdapterDungeon(this, dungeonModels);
+        listViewDungeon.setAdapter(adapterDungeon);
 
 
         final AsyncHttpClient client = new AsyncHttpClient();
@@ -49,53 +111,7 @@ public class Dungeon extends AppCompatActivity {
         // Not sure why this boolean isn't working but the objective was to make it so that if a user enter's nothing, the search doesn't happen
 
 
-        client.get("https://us.api.battle.net/wow/zone/?locale=en_US&apikey=wheces9zargz65mhza5jfv9nentuy2gg", new JsonHttpResponseHandler() {
-
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject responseBody) {
-
-
-                try {
-                    JSONArray jsonArray = responseBody.getJSONArray("zones");
-
-
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject dungeon = jsonArray.getJSONObject(i);
-                        if (!dungeon.has("name")) continue;
-                        items.add(dungeon.getString("name"));
-                    }
-
-                    mAdapter.notifyDataSetChanged();
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                super.onFailure(statusCode, headers, throwable, errorResponse);
-                Toast.makeText(getApplicationContext(), "Process Not Successful",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        //            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//                @Override
-//                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                    Intent myIntent = new Intent(Bosses.this, Main2Activity.class);
-//                    myIntent.putExtra("position", position);
-//                    String imageid = items.get(position);
-//                    String picasso = items.get(position+1);
-//                    myIntent.putExtra("url", imageid);
-//                    myIntent.putExtra("url2", picasso);
-//                    startActivity(myIntent);
-//                }
-//            });
-
+        client.get("https://us.api.battle.net/wow/zone/?locale=en_US&apikey=wheces9zargz65mhza5jfv9nentuy2gg", jsonHttpResponseHandler);
 
 
     }
