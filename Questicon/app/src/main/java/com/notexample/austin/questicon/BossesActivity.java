54aa70998a1c3a6fc0d55f3b2abb8485441d5900
aharshbe <a.catawampus@gmail.com>
2016-06-14 19:41:53 -0700

    Adding the xml for the searchable thing

--- Questicon/app/src/main/java/com/notexample/austin/questicon/BossesActivity.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/BossesActivity.java
@@ -1,8 +1,11 @@
 package com.notexample.austin.questicon;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -100,8 +103,6 @@ public class BossesActivity extends AppCompatActivity {
     }
 
 
-
-
     public void bosses() {
 
         ListView listViewBosses = (ListView) findViewById(R.id.listViewBosses);
@@ -144,6 +145,13 @@ public class BossesActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
+
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
 
 
         return true;
