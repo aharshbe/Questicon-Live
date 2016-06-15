128d44e7d82ed8158491f287278df8297a928d6d
aharshbe <a.catawampus@gmail.com>
2016-06-15 16:18:46 -0700

    Adding the working search functionality of the pets acitivty

--- Questicon/app/src/main/java/com/notexample/austin/questicon/PetsActivity.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/PetsActivity.java
@@ -3,6 +3,9 @@ package com.notexample.austin.questicon;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,6 +27,8 @@ import cz.msebera.android.httpclient.Header;
 public class PetsActivity extends AppCompatActivity {
     ArrayList<PetsModel> petsModels;
     CustomAdapterPets adapterPets;
+    String theySearched = "";
+    ArrayList<PetsModel> petsModels2;
     JsonHttpResponseHandler jsonHttpResponseHandler = new JsonHttpResponseHandler() {
 
 
@@ -48,19 +53,33 @@ public class PetsActivity extends AppCompatActivity {
                     ListView listViewPets = (ListView) findViewById(R.id.listViewPets);
 
 
+                    assert listViewPets != null;
                     listViewPets.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
                         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                            PetsModel pet = petsModels.get(position);
 
-                            Intent myIntent = new Intent(PetsActivity.this, PetDetailView.class);
-                            myIntent.putExtra("position", position);
-                            myIntent.putExtra("name", pet.getNamePet());
-                            myIntent.putExtra("imageurl", pet.getImagePet());
-                            startActivity(myIntent);
+                            if (theySearched.equalsIgnoreCase("")) {
+                                PetsModel pet = petsModels.get(position);
+                                Intent myIntent = new Intent(PetsActivity.this, PetDetailView.class);
+                                myIntent.putExtra("position", position);
+                                myIntent.putExtra("name", pet.getNamePet());
+                                myIntent.putExtra("imageurl", pet.getImagePet());
+                                startActivity(myIntent);
+                            }else{
+                                PetsModel pet = petsModels2.get(position);
+                                Intent myIntent = new Intent(PetsActivity.this, PetDetailView.class);
+                                myIntent.putExtra("position", position);
+                                myIntent.putExtra("name", pet.getNamePet());
+                                myIntent.putExtra("imageurl", pet.getImagePet());
+                                startActivity(myIntent);
+
+                            }
+
+
                         }
                     });
+
                 }
 
 
@@ -106,5 +125,51 @@ public class PetsActivity extends AppCompatActivity {
         client.get("https://us.api.battle.net/wow/pet/?locale=en_US&apikey=wheces9zargz65mhza5jfv9nentuy2gg", jsonHttpResponseHandler);
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                theySearched = newText;
+
+                petsModels2 = new ArrayList<>();
+
+                for (int i = 0; i < petsModels.size(); i++) {
+
+                    if (petsModels.get(i).getNamePet().toLowerCase().contains(newText.toLowerCase())) {
+                        petsModels2.add(petsModels.get(i));
+                    }
+
+                }
+                ListView listViewPets = (ListView) findViewById(R.id.listViewPets);
+                petsModels.contains(newText);
+                adapterPets = new CustomAdapterPets(PetsActivity.this, petsModels2);
+                listViewPets.setAdapter(adapterPets);
+
+                adapterPets.notifyDataSetChanged();
+
+
+                return false;
+            }
+        });
+
+
+        return true;
     }
 }
