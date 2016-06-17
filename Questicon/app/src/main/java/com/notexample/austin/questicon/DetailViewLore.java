747c7ba5bd39690f4cd82321c43ded355f4fa252
aharshbe <a.catawampus@gmail.com>
2016-06-17 10:12:59 -0700

    Adding the share functionality to the lore acitivty

--- Questicon/app/src/main/java/com/notexample/austin/questicon/DetailViewLore.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/DetailViewLore.java
@@ -1,9 +1,13 @@
 package com.notexample.austin.questicon;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -51,5 +55,18 @@ public class DetailViewLore extends AppCompatActivity {
         URLString2 = URLString.replaceAll("\\s+", "_");
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.share, menu);
+        return true;
+    }
+    public void clickingShare(MenuItem item) {
+
+        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT,URLString2);
+        startActivity(Intent.createChooser(sharingIntent, getString(R.string.send_intent_title)));
     }
 }
