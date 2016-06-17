8f871c602e37a0d4a4307068c56f024164d770f0
aharshbe <a.catawampus@gmail.com>
2016-06-17 11:30:25 -0700

    got this working but going to clean it up

--- Questicon/app/src/main/java/com/notexample/austin/questicon/MusicActivity.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/MusicActivity.java
@@ -1,13 +1,38 @@
 package com.notexample.austin.questicon;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MusicActivity extends AppCompatActivity {
+     MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+
+       mp = MediaPlayer.create(this, R.raw.a1);
+
+
+    }
+
+
+    public void playbutton(View view) {
+        mp.start();
+    }
+
+    public void stop(View view) {
+        mp.stop();
+    }
+
+    public void download(View view) {
+        String url = "https://docs.google.com/uc?export=download&id=0B7NezlAJoyHJcHdGWkd6UE16Y0k";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
