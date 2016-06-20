23334469e0ed160481992d428635d020c5cf6b13
aharshbe <a.catawampus@gmail.com>
2016-06-20 15:32:38 -0700

    modifying some of the things for the chooser to make them look a little nicer

--- Questicon/app/src/main/java/com/notexample/austin/questicon/HorADetailView.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/HorADetailView.java
@@ -36,6 +36,8 @@ public class HorADetailView extends AppCompatActivity {
 
     public void AllianceInfoDialogue() {
         AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_alliancecresticon);
+        builder1.setTitle("The Alliance");
         builder1.setMessage("Humans, night elves, dwarves, gnomes, draenei, worgen, and most recently, pandaren (Tushui) make up the illustrious Alliance.'\n" +
                 "Proud and noble, courageous and wise, these races work together to preserve order in Azeroth. The Alliance is driven by honor and tradition. Its rulers are champions of justice, hope, knowledge, and faith.\n" +
                 "In a time when chaos and uncertainty reign, the Alliance remains steadfast in its determination to bring light to the darkest corners of the world.");
@@ -57,6 +59,8 @@ public class HorADetailView extends AppCompatActivity {
     }
     public void HordeInfoDialogue() {
         AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_hordecresticon);
+        builder1.setTitle("The Horde");
         builder1.setMessage("The Horde is made up of Orcs, Forsaken, Tauren, Trolls, Blood Elves, Goblins, and most recently, Pandaren (Huojin). Misunderstood and cast aside, these diverse and powerful races strive to overcome their differences and unite as one in order to win freedom for their people and prosper in a land that has come to hate them.\n" +
                 "In the Horde, action and strength are valued above diplomacy, and its leaders earn respect by the blade, wasting no time with politics. The brutality of the Horde's champions is focused, giving a voice to those who fight for survival.");
         builder1.setCancelable(true);
