dd4b3597782ee69466478d93c4ba237137ac3934
aharshbe <a.catawampus@gmail.com>
2016-06-20 12:06:14 -0700

    Adding the worgen and pandaring logic and icons with dialogues

--- Questicon/app/src/main/java/com/notexample/austin/questicon/AllianceSelectActivity.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/AllianceSelectActivity.java
@@ -21,7 +21,7 @@ public class AllianceSelectActivity extends AppCompatActivity {
 
     public void clickingFramePandarin(View view) {
         AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-        builder1.setIcon(R.mipmap.ic_launcher_questicon);
+        builder1.setIcon(R.mipmap.ic_pandaren);
         builder1.setTitle("Pandaren: The Gentle");
         builder1.setMessage("Couched in myth and legend, rarely seen and even more rarely understood, the enigmatic pandaren have long been a mystery to the other races of Azeroth. The noble history of the pandaren people stretches back thousands of years, well before the empires of man and before even the sundering of the world.");
         builder1.setCancelable(true);
@@ -56,6 +56,54 @@ public class AllianceSelectActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int id) {
                         Intent browserIntent = new Intent(Intent.ACTION_VIEW);
                         browserIntent.setData(Uri.parse("https://www.youtube.com/watch?v=UQh94o_Ph_I"));
+                        startActivity(browserIntent);
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingFrameWorgen(View view) {
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_worgenreal);
+        builder1.setTitle("Worgen: The Changed");
+        builder1.setMessage("Behind the formidable Greymane Wall, a terrible curse has spread throughout the isolated human nation of Gilneas, transforming many of its stalwart citizens into nightmarish beasts known as worgen. The origins of this curse have been fiercely debated, but only recently has the startling truth come to light.");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Select Worgen",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(AllianceSelectActivity.this, AllianceSelectActivity.class);
+                        startActivity(intent);
+
+
+                        return;
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "Further info",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+                        browserIntent.setData(Uri.parse("http://us.battle.net/wow/en/game/race/worgen"));
+                        startActivity(browserIntent);
+
+                        return;
+                    }
+                });
+
+        builder1.setNeutralButton(
+                "Worgen Video",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+                        browserIntent.setData(Uri.parse("https://www.youtube.com/watch?v=IorM2yfiZCI"));
                         startActivity(browserIntent);
 
                         return;
