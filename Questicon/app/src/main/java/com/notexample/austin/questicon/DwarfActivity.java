d2eb6269284adbe8c9c2a50135fc8b77b7f69734
aharshbe <a.catawampus@gmail.com>
2016-06-22 14:47:43 -0700

    getting the dwarf activity working

--- Questicon/app/src/main/java/com/notexample/austin/questicon/DwarfActivity.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/DwarfActivity.java
@@ -1,5 +1,7 @@
 package com.notexample.austin.questicon;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -94,5 +96,85 @@ public class DwarfActivity extends AppCompatActivity {
         mediaPlayer2.stop();
         mediaPlayer2.reset();
         mediaPlayer2.release();
+    }
+
+    public void clickingStoneForm(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_stoneform);
+        builder1.setTitle("STONEFORM");
+        builder1.setMessage("Dwarves can temporarily turn to living stone, neutralizing any poisons, diseases, and bleeding wounds, and adding natural armor.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingFrostRes(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_frostresist);
+        builder1.setTitle("FROST RESISTANCE");
+        builder1.setMessage("Ironforge dwarves have a natural resistance to Frost magic.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingExporer(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_exporer);
+        builder1.setTitle("EXPLORER");
+        builder1.setMessage("Dwarves receive a skill bonus to archaeology and are able to survey faster than other races.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingMightof(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_mightofmountain);
+        builder1.setTitle("MIGHT OF THE MOUNTAIN");
+        builder1.setMessage("Dwarves battle with powerful intent and gain a bonus to critical strikes.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
