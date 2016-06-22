03ce54d1b1ba5f0a568eeba9431d578319d6c4c3
aharshbe <a.catawampus@gmail.com>
2016-06-22 13:42:50 -0700

    got the blood elf clicks working

--- Questicon/app/src/main/java/com/notexample/austin/questicon/BloodElfAcitivty.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/BloodElfAcitivty.java
@@ -1,5 +1,7 @@
 package com.notexample.austin.questicon;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -95,5 +97,87 @@ public class BloodElfAcitivty extends AppCompatActivity {
         mediaPlayer2.stop();
         mediaPlayer2.reset();
         mediaPlayer2.release();
+    }
+
+    public void clickingArcaneA(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_acraneacunity);
+        builder1.setTitle("ARCANE ACUITY");
+        builder1.setMessage("Blood elves possess a preternatural talent for precision and gain a bonus to critical strikes.");
+
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingArcaneTorrent(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_arcanetorrent);
+        builder1.setTitle("ARCANE TORRENT");
+        builder1.setMessage("Blood elves can disrupt magic, briefly silencing all enemy casters around them and restoring some of their own resources.");
+
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingArcaneAffinity(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_arcaneaffinity);
+        builder1.setTitle("ARCANE AFFINITY");
+        builder1.setMessage("Blood elves are skilled in the mystical arts and receive a skill bonus to enchanting.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingMagicResist(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_magicresist);
+        builder1.setTitle("MAGIC RESISTANCE");
+        builder1.setMessage("Blood elves have a natural resistance to magic spells.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
