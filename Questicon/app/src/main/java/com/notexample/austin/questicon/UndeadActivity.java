c888d2706e9cd6db4c8b41c92536119ea34e04da
aharshbe <a.catawampus@gmail.com>
2016-06-22 14:17:04 -0700

    adding the undead clicks working

--- Questicon/app/src/main/java/com/notexample/austin/questicon/UndeadActivity.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/UndeadActivity.java
@@ -1,5 +1,7 @@
 package com.notexample.austin.questicon;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Handler;
@@ -99,5 +101,105 @@ public class UndeadActivity extends AppCompatActivity {
         mediaPlayer2.stop();
         mediaPlayer2.reset();
         mediaPlayer2.release();
+    }
+
+    public void clickingCanna(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_cann);
+        builder1.setTitle("CANNIBALIZE");
+        builder1.setMessage("One rather grisly means for the Forsaken to replenish their health is by consuming dead bodies.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingTouchofgrave(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_touchofthegrave);
+        builder1.setTitle("TOUCH OF THE GRAVE");
+        builder1.setMessage("The Forsaken can sometimes drain the life from their enemies and use it to repair their own bodies.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingShadowResist(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_shadowresistundead);
+        builder1.setTitle("SHADOW RESISTANCE");
+        builder1.setMessage("The Forsaken have a natural resistance to Shadow magic.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingWillOf(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_willoftheforesaken);
+        builder1.setTitle("WILL OF THE FORSAKEN");
+        builder1.setMessage("The Forsaken can shake off any charm, fear and sleep effects at will.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingUnderwaterBreath(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_underwaterbreath);
+        builder1.setTitle("UNDERWATER BREATHING");
+        builder1.setMessage("The Forsaken’s undead bodies need less air than those of the living, allowing them to hold their breath longer.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
