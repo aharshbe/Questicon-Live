eb708efe404d824ded6a2e069f3aa34739dd55e2
aharshbe <a.catawampus@gmail.com>
2016-06-22 14:06:12 -0700

    got the troll acitivty working

--- Questicon/app/src/main/java/com/notexample/austin/questicon/TrollAcitivty.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/TrollAcitivty.java
@@ -1,5 +1,7 @@
 package com.notexample.austin.questicon;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -93,5 +95,105 @@ public class TrollAcitivty extends AppCompatActivity {
         mediaPlayer2.stop();
         mediaPlayer2.reset();
         mediaPlayer2.release();
+    }
+
+    public void clickingVoodoo(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_vodoo);
+        builder1.setTitle("DA VOODOO SHUFFLE");
+        builder1.setMessage("Thanks to their impressive legerity, trolls are less affected by movement impairing effects than other races.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingRegeneration(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_regeneration);
+        builder1.setTitle("REGENERATION");
+        builder1.setMessage("Trolls have a natural healing factor that constantly replenishes their health, even when they are engaged in combat.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingBeastSlay(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_bestslay);
+        builder1.setTitle("BEAST SLAYING");
+        builder1.setMessage("Trolls hunt with a keen awareness and gain additional experience from killing beasts.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingRanged(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_ranged);
+        builder1.setTitle("THROWING WEAPON AND BOW SPECIALIZATION");
+        builder1.setMessage("Trolls receive bonuses when using their favored weapons.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingBerserking(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_berserking);
+        builder1.setTitle("BERSERKING");
+        builder1.setMessage("Trolls can launch themselves into a frenzy, increasing their attack and casting speed for a short time.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
