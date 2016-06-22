0c171d0fbaa3b94528a2e7b43303ed0aae66e19a
aharshbe <a.catawampus@gmail.com>
2016-06-22 14:33:57 -0700

    Adding the working draenei clicks

--- Questicon/app/src/main/java/com/notexample/austin/questicon/DraeneiAcitivity.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/DraeneiAcitivity.java
@@ -1,5 +1,7 @@
 package com.notexample.austin.questicon;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -94,5 +96,85 @@ public class DraeneiAcitivity extends AppCompatActivity {
         mediaPlayer2.stop();
         mediaPlayer2.reset();
         mediaPlayer2.release();
+    }
+
+    public void clickingGiftofNaru(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_giftofnaru);
+        builder1.setTitle("GIFT OF THE NAARU");
+        builder1.setMessage("The draenei are blessed with a healing ability that can restore their health and that of their allies.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingShadowResist2(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_shadowresistundead);
+        builder1.setTitle("SHADOW RESISTANCE");
+        builder1.setMessage("Draenei have a natural resistance to Shadow magic.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingHeroicPresence(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_heroicpresence);
+        builder1.setTitle("HEROIC PRESENCE");
+        builder1.setMessage("Confident and strong, draenei receive a slight bonus to their main stat.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingGemSpe(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_gemcutting);
+        builder1.setTitle("GEMCUTTING");
+        builder1.setMessage("The talented draenei receive a bonus to their jewelcrafting skill.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
