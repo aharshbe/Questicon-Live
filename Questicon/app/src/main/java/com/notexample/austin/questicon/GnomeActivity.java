c7dea0cf3fdce550ecf61a0088d7e464a77d154d
aharshbe <a.catawampus@gmail.com>
2016-06-22 14:57:33 -0700

    adding the working clicks for the gnomes

--- Questicon/app/src/main/java/com/notexample/austin/questicon/GnomeActivity.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/GnomeActivity.java
@@ -1,5 +1,7 @@
 package com.notexample.austin.questicon;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -94,5 +96,105 @@ public class GnomeActivity extends AppCompatActivity {
         mediaPlayer2.stop();
         mediaPlayer2.reset();
         mediaPlayer2.release();
+    }
+
+    public void clickingEscapeArtist(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_escapeartist);
+        builder1.setTitle("ESCAPE ARTIST");
+        builder1.setMessage("Gnomes are small and nimble, which allows them to extricate themselves from even the most cunning traps.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingArcaneResist(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_arcanetorrent);
+        builder1.setTitle("ARCANE RESISTANCE");
+        builder1.setMessage("Gnomes have a natural resistance to Arcane magic.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingExpansiveMind(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_expansivemind);
+        builder1.setTitle("EXPANSIVE MIND");
+        builder1.setMessage("Curious, creative, and brilliant, gnomes receive a small bonus to their mana pool.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingEngSpec(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_engspecial);
+        builder1.setTitle("ENGINEERING SPECIALIZATION");
+        builder1.setMessage("Their expertise with gizmos, gadgets, and gimmicks provides gnomes with a skill bonus to engineering.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingNimble(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_nimblefingers);
+        builder1.setTitle("NIMBLE FINGERS");
+        builder1.setMessage("Gnomes, while diminutive in stature, are dangerously quick.");
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
