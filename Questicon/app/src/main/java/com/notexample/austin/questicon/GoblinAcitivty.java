2d65e3904b738dee3838d0e500402d8defeeb078
aharshbe <a.catawampus@gmail.com>
2016-06-22 12:29:30 -0700

    Adding on click for goblin racials

--- Questicon/app/src/main/java/com/notexample/austin/questicon/GoblinAcitivty.java
+++ Questicon/app/src/main/java/com/notexample/austin/questicon/GoblinAcitivty.java
@@ -1,5 +1,7 @@
 package com.notexample.austin.questicon;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -94,5 +96,131 @@ public class GoblinAcitivty extends AppCompatActivity {
         mediaPlayer2.stop();
         mediaPlayer2.reset();
         mediaPlayer2.release();
+    }
+
+    public void clickingRocketJump(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_rocketjump);
+        builder1.setTitle("ROCKET JUMP");
+        builder1.setMessage("Goblins can use their trusty (well, relatively speaking) rocket belts to launch themselves over short distances.");
+
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingRocketBar(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_rocketbar);
+        builder1.setTitle("ROCKET BARRAGE");
+        builder1.setMessage("The rocket belt can also launch rockets at an enemy, causing fire damage to the target.");
+
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingHobGoblin(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_hobgob);
+        builder1.setTitle("PACK HOBGOBLIN");
+        builder1.setMessage("Goblins can access their bank vault from anywhere with the help of a trusted friend.");
+
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingDeals(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_extradeals);
+        builder1.setTitle("BEST DEALS ANYWHERE");
+        builder1.setMessage("Wheeling and dealing is second nature to goblins, and they always receive a discount from vendors.");
+
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingTimeisMoney(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_timeismoney);
+        builder1.setTitle("TIME IS MONEY");
+        builder1.setMessage("Goblins cash in on a bonus to haste.");
+
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    public void clickingChemisry(View view) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setIcon(R.mipmap.ic_chemistry);
+        builder1.setTitle("BETTER LIVING THROUGH CHEMISTRY");
+        builder1.setMessage("Goblins have a natural affinity for fragile, hazardous materials and receive a bonus to Alchemy.");
+
+        builder1.setPositiveButton(
+                "Cool!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        return;
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
